3433ca0fcec36a771bd88b713204f278758d9aa9
CaiShuangLian <2243157861@qq.com>
2021-08-11 07:46:47 +0800

    链表节点

---
+++ notes/src/csl/offerstudy/linklist/ListNode.java
@@ -0,0 +1,17 @@
+package csl.offerstudy.linklist;
+
+/**
+ * @Author:CaiShuangLian
+ * @FileName:
+ * @Date:Created in  2021/8/10 18:21
+ * @Version:
+ * @Description:链表节点
+ */
+
+public class ListNode {
+    int val;
+    ListNode next=null;
+    ListNode(int val){
+        this.val=val;
+    }
+}
